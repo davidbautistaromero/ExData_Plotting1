10251e7befffe4eecafc89b77f5e316b9652d394
David Bautista <ds.bautista1998@gmail.com>
2020-10-04 21:40:43 -0500

    Course Project Plot 2

---
+++ plot2.R
@@ -0,0 +1,16 @@
+##plot 2
+
+data <- read.table("household_power_consumption.txt",
+                   header = TRUE, sep = ";", stringsAsFactors = FALSE,
+                   dec = ".")
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+sapply(data, class)
+data$Global_active_power <- as.numeric(data$Global_active_power)
+subsetdata$datetime <- strptime(paste(subsetdata$Date, subsetdata$Time,
+                                      sep = " "), "%d/%m/%Y %H:%M:%S")
+
+png("plot2.png", width = 450, height = 450)
+with(subsetdata, plot(datetime, Global_active_power, type = "l",
+                      xlab = "", ylab = "Global Active Power (kilowatts)"))
+dev.off()
