4b41cd63d791d28ed3d7d697e9b664c115ce0dab
David Bautista <ds.bautista1998@gmail.com>
2020-10-04 22:01:12 -0500

    Course Project Plot 3

--- plot3.R
+++ plot3.R
@@ -21,5 +21,8 @@ lines(subsetdata$datetime, subsetdata$Sub_metering_2,
       type = "l", col = "red")
 lines(subsetdata$datetime, subsetdata$Sub_metering_3, 
       type = "l", col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2",
+                              "Sub_metering_3"),
+       col = c("black", "red", "blue"), lty = 1, lwd = 2)
 
 dev.off()
