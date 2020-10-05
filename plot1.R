df208c73749983b7c241f795706c8f018488ae96
David Bautista <ds.bautista1998@gmail.com>
2020-10-04 21:27:28 -0500

    Course Project Plot 1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+## plot 1
+
+data <- read.table("household_power_consumption.txt",
+                   header = TRUE, sep = ";", stringsAsFactors = FALSE,
+                   dec = ".")
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+sapply(data, class)
+data$Global_active_power <- as.numeric(data$Global_active_power)
+png("plot1.png", width = 450, height = 450)
+with(subsetdata, hist(Global_active_power, col = "red", 
+                      main = "Global Active Power", 
+                      xlab = "Global Active Power (kilowatts)"))
+dev.off()
