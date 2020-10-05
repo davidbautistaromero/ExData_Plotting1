28acc362013d560e12d0184563d885f66f7529d2
David Bautista <ds.bautista1998@gmail.com>
2020-10-04 22:27:54 -0500

    Course Project Plot 1

--- plot1.R
+++ plot1.R
@@ -7,7 +7,7 @@ subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 sapply(data, class)
 data$Global_active_power <- as.numeric(data$Global_active_power)
-png("plot1.png", width = 450, height = 450)
+png("plot1.png", width = 480, height = 480)
 with(subsetdata, hist(Global_active_power, col = "red", 
                       main = "Global Active Power", 
                       xlab = "Global Active Power (kilowatts)"))
